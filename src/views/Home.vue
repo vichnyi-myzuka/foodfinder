c2ffbad3e210781e276f9778730c76baa10ea64f
vicnhyi_myzuka <62535515+vichnyimuzika@users.noreply.github.com>
2021-06-13 23:35:15 +0300

    Main

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    <h1>Останні страви</h1>
+    <h1>Вас вітає FoodFinder!</h1>
+    <p>
+      Наш сервіс допоможе вам правильно розрахувати кількість продуктів, що вам треба
+      купити, для приготування страви!
+    </p>
+    <h2>
+      Перейдіть до розділів, щоб додати страви
+    </h2>
+    <v-btn color="success" @click="setDrawer(true)" elevation="0" class="button">Перейти</v-btn>
     <ToCartButton/>
   </div>
 </template>
@@ -8,10 +16,14 @@
 <script>
 import ToCartButton from '../components/Main/Buttons/ToCartButton/ToCartButton'
 import Vue from 'vue'
+import { mapActions } from 'vuex'
 export default Vue.extend({
   name: 'Home',
   components: {
     ToCartButton
+  },
+  methods: {
+    ...mapActions(['setDrawer'])
   }
 })
 </script>
